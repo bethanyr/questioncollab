d02a9417e4cd7bbab33481de9efdc170a213ec1f
bethanyr <bethany.rentz@hotmail.com>
2013-07-22 13:17:57 -0700

    initial version of application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Questioncollab::Application.config.secret_token = '0a35de4e6c847f2096f334e73eca350a8f81837a6f917664db2ac59d46798e913c92e5d5102dc81d027226353f4e6f89ff24b939f453c499c33ed1ad86758632'
