af7f21215942105e782168c5993c929c4592f102
jzhang20133 <jzhang906@gmail.com>
2013-07-23 22:30:16 -0700

    add sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   def show
+    @question = Question.find(params[:question_id])
+    @user = @question.users.find(params[:id])
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,15 @@
 user = User.create(:email => "admin@ticketee.com",
 :password => "password")
 user.confirm!
+user2 = User.create(:email => "admin2@ticketee.com",
+:password => "password")
+user2.confirm!
+user3 = User.create(:email => "admin3@ticketee.com",
+:password => "password")
+user3.confirm!
+user4 = User.create(:email => "admin4@ticketee.com",
+:password => "password")
+user4.confirm!
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
